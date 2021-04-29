491dbf20ca4dbf09a5dfbef471e9f136e740f616
Paolo D'Amico <paolodamico@users.noreply.github.com>
2021-04-28 18:45:38 -0700

    move to production

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
-export const DEBUG = true;
+export const DEBUG = false;
 export const MAX_UINT256 =
   "115792089237316195423570985008687907853269984665640564039457584007913129639935";
 export const NETWORK = DEBUG ? "kovan" : "mainnet";
